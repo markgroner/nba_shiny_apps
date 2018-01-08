f009002753aed62ae508193704755481f50bfea2
Mark Groner <markgroner@utexas.edu>
2018-01-08 16:17:50 -0600

    fixed nba api request and dynamically set response columns to numeric

--- nba_api_call_functions.R
+++ nba_api_call_functions.R
@@ -12,32 +12,39 @@ nba_api_request <- function(api_url, parameters, headers) {
     )
   request_status <- stop_for_status(request)
   data <- content(request)
-
-  raw_data <- data$resultSets[[1]]$rowSet
-  col_names <- as.character(data$resultSets[[1]]$headers)
-
-  if (length(raw_data) == 0) {
-      response_df <- data.frame(
-          matrix(nrow = 0, ncol = length(col_names))
-      )
-  } else {
-      response_df = data.frame(
-          matrix(
-              unlist(raw_data),
-              ncol = length(col_names),
-              byrow = TRUE
-          )
-      )
-  }
-
-  response_df <- tbl_df(response_df)
-  names(response_df) <- col_names
-  response_df <- mutate(response_df,
-                        PERIOD = as.numeric(as.character(PERIOD)),
-                        SHOT_DISTANCE = as.numeric(as.character(SHOT_DISTANCE)),
-                        SHOT_MADE_NUMERIC = as.numeric(as.character(SHOT_MADE_FLAG)),
-                        SHOT_ATTEMPTED_FLAG = as.numeric(as.character(SHOT_ATTEMPTED_FLAG)))
+  main_result_json <- data$resultSets[[1]]
+  rowset_data <- main_result_json$rowSet
+  column_headers <- as.character(main_result_json$headers)
+  response_df = data.frame(
+      matrix(
+          unlist(rowset_data),
+          ncol = length(column_headers),
+          byrow = TRUE
+      ),
+      stringsAsFactors = FALSE)
+  names(response_df) <- column_headers
+  response_df <- set_numeric_column_type(response_df, .9)
   return(response_df)
+}
+
+
+## numeric_threshold if this percentage of values in a column can be coerced to numeric, switch column type
+set_numeric_column_type <- function(df, numeric_threshold) {
+  for (column_name in colnames(df)) {
+    name_length <- nchar(column_name)
+    if (substr(column_name, name_length - 2, name_length) == '_ID') {
+      ## pass
+    } else {
+        numeric_test_column <- as.numeric(df[[column_name]])
+        total_values <- NROW(numeric_test_column)
+        non_na_values <- NROW(na.omit(numeric_test_column))
+        perc_non_na <- non_na_values / total_values
+        if (perc_non_na >= numeric_threshold) {
+          df[[column_name]] <- numeric_test_column
+        }
+    }
+  }
+  return(df)
 }
 
 kyrie_player_id <- 202681
@@ -70,5 +77,5 @@ headers <- c('Accept-Language' = 'en-US,en;q=0.5',
               Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
               Referer = 'http://markgroner.com',
               Connection = 'keep-alive')
-df <- nba_api_request(shot_chart_url, kyrie_query, headers)
-print(df)
+response_df <- nba_api_request(shot_chart_url, kyrie_query, headers)
+print(response_df)
